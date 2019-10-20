a118d05868345e298af7cc204569bb09952b7a07
zolotar <irinaspns@gmail.com>
2019-10-20 17:53:09 +0200

    Init commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,66 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage,null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid != null &&  storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int removeIndex = -1;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid != null &&  storage[i].uuid.equals(uuid)) {
+                removeIndex = i;
+                break;
+            }
+        }
+        if (removeIndex >= 0) {
+            for(int i = removeIndex; i < storage.length -1; i++){
+                storage[i] = storage[i + 1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int lastFilledIndex = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                lastFilledIndex = i;
+            } else {
+                break;
+            }
+        }
+        return Arrays.copyOf(storage, ++lastFilledIndex);
     }
 
     int size() {
-        return 0;
+        int storageSize = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storageSize = i;
+            } else {
+                break;
+            }
+        }
+        return ++storageSize;
     }
 }

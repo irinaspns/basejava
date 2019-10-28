a0034104ffc5e0f58ac74407388651573144638c
zolotar <irinaspns@gmail.com>
2019-10-28 18:31:44 +0100

    Refactoring

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -13,6 +13,7 @@ public class ArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     public void update(Resume resume) {
@@ -52,6 +53,8 @@ public class ArrayStorage {
             }
             storage[size - 1] = null;
             size--;
+        } else {
+            System.out.println("Nothing to delete. No Resume with uuid = " + uuid + " in storage.");
         }
     }
 

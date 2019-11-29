4e3fc5f5c87b00deacce783d2398da57d509186f
zolotar <irinaspns@gmail.com>
2019-11-29 19:35:37 +0100

    Lesson07 enum

---
+++ src/com/urise/webapp/model/SectionType.java
@@ -0,0 +1,20 @@
+package com.urise.webapp.model;
+
+public enum SectionType {
+    PERSONAL("Personal qualities"),
+    OBJECTIVE("Position"),
+    ACHIEVEMENT("Achievements"),
+    QUALIFICATIONS("Qualification"),
+    EXPERIENCE("Experience"),
+    EDUCATION("Education");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}

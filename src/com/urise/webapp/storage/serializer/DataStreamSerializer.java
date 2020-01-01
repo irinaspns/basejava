6496eabf70743c6b4a3a6e59c98350dc361a6c51
zolotar <irinaspns@gmail.com>
2020-01-01 22:15:11 +0100

    HW-10 temp. commit

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -1,11 +1,10 @@
 package com.urise.webapp.storage.serializer;
 
-import com.urise.webapp.model.ContactType;
-import com.urise.webapp.model.Resume;
-import com.urise.webapp.model.Section;
-import com.urise.webapp.model.SectionType;
+import com.urise.webapp.model.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
@@ -23,11 +22,38 @@ public class DataStreamSerializer implements StreamSerializer {
             }
             // TODO implements sections
             Map<SectionType, Section> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
+
+            writeTextSection(SectionType.OBJECTIVE, sections, dos);
+            writeTextSection(SectionType.PERSONAL, sections, dos);
+
+            writeTextListSection(SectionType.ACHIEVEMENT, sections, dos);
+            writeTextListSection(SectionType.QUALIFICATIONS, sections, dos);
+        }
+    }
+    private void writeTextSection(SectionType sectionType,
+                                      Map<SectionType, Section> sections,
+                                      DataOutputStream dos) throws IOException {
+        Section section = sections.get(sectionType);
+        dos.writeInt(section == null ? 0 : 1);
+        if (section != null) {
+            dos.writeUTF(sectionType.name());
+            dos.writeUTF(((TextSection) sections.get(sectionType)).getText());
+        }
+    }
+
+    private void writeTextListSection(SectionType sectionType,
+                                      Map<SectionType, Section> sections,
+                                      DataOutputStream dos) throws IOException {
+        Section section = sections.get(sectionType);
+        dos.writeInt(section == null ? 0 : 1);
+        dos.writeUTF(sectionType.name());
+        if (section != null) {
+            TextListSection textListSection = (TextListSection) sections.get(sectionType);
+            List<String> list = textListSection.getList();
+            dos.writeInt(list.size());
+            for (String s : list) {
+                dos.writeUTF(s);
             }
-            r.getSection(SectionType.EDUCATION);
         }
     }
 
@@ -42,7 +68,31 @@ public class DataStreamSerializer implements StreamSerializer {
                 resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
             // TODO implements sections
+            if (dis.readInt() > 0) { // OBJECTIVE
+                resume.addSection(SectionType.valueOf(dis.readUTF()), new TextSection(dis.readUTF()));
+            }
+            if (dis.readInt() > 0) { // PERSONAL
+                resume.addSection(SectionType.valueOf(dis.readUTF()), new TextSection(dis.readUTF()));
+            }
+
+            if (dis.readInt() > 0) { // ACHIEVEMENT
+                readTextListSection(dis, resume);
+            }
+            if (dis.readInt() > 0) { // QUALIFICATIONS
+                readTextListSection(dis, resume);
+            }
+
             return resume;
         }
+    }
+
+    private void readTextListSection(DataInputStream dis, Resume resume) throws IOException {
+        SectionType sectionType = SectionType.valueOf(dis.readUTF());
+        int size = dis.readInt();
+        List<String> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(dis.readUTF());
+        }
+        resume.addSection(sectionType, new TextListSection(list));
     }
 }

79baeb1d56cf98eccc4669ce69dcde77fdb1c19c
zolotar <irinaspns@gmail.com>
2020-01-16 20:29:55 +0100

    HW-12 Streams.

---
+++ src/com/urise/webapp/MainStreams.java
@@ -0,0 +1,38 @@
+package com.urise.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainStreams {
+
+    public static void main(String[] args) {
+        int[] a =  {1, 2, 3, 3, 2, 3} ;
+        System.out.println(minValue(a));
+
+        List <Integer> integers = Arrays.asList(5, 7, 8, 3, 2, 5);
+        System.out.println(oddOrEven(integers));
+    }
+
+    private static int minValue(int[] values) {
+        String value = IntStream.of(values)
+                .distinct()
+                .sorted()
+                .mapToObj(v -> Integer.valueOf(v).toString())
+                .reduce("", (partialString, element) -> partialString + element);
+
+        return Integer.parseInt(value);
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        List <Integer> result = integers
+                .stream()
+                .reduce(0, Integer::sum) % 2 == 0 ?
+                    integers.stream().filter(p -> p % 2 != 0).collect(Collectors.toList())
+                    :
+                    integers.stream().filter(p1 -> p1 % 2 == 0).collect(Collectors.toList());
+        return result;
+    }
+
+}

85227354184832c2b63ebe3d069d8d6ce675fc8f
zolotar <irinaspns@gmail.com>
2020-01-17 19:32:06 +0100

    HW-12 Refactoring streams

--- src/com/urise/webapp/MainStreams.java
+++ src/com/urise/webapp/MainStreams.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 public class MainStreams {
 
     public static void main(String[] args) {
-        int[] a =  {1, 2, 3, 3, 2, 3} ;
+        int[] a =  {1, 2, 3, 3, 4, 2, 3} ;
         System.out.println(minValue(a));
 
         List <Integer> integers = Arrays.asList(5, 7, 8, 3, 2, 5);
@@ -16,19 +16,17 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        String value = IntStream.of(values)
-                .distinct()
-                .sorted()
-                .mapToObj(v -> Integer.valueOf(v).toString())
-                .reduce("", (partialString, element) -> partialString + element);
-
-        return Integer.parseInt(value);
+        return IntStream.of(values)
+               .distinct()
+               .sorted()
+               .reduce(0, (subSum, number) -> subSum *10 + number);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         return integers.stream().filter(integers
                 .stream()
-                .reduce(0, Integer::sum) % 2 == 0 ? p -> p % 2 != 0 : p1 -> p1 % 2 == 0).collect(Collectors.toList());
+                .reduce(0, Integer::sum) % 2 == 0 ? p -> p % 2 != 0 : p1 -> p1 % 2 == 0)
+                .collect(Collectors.toList());
     }
 
 }

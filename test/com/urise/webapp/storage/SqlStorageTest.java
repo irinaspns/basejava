d18c3f3ddbd392fd48969af36faee411dc3516e9
zolotar <irinaspns@gmail.com>
2020-01-26 11:50:43 +0100

    HW-13 Refactoring

--- test/com/urise/webapp/storage/SqlStorageTest.java
+++ test/com/urise/webapp/storage/SqlStorageTest.java
@@ -3,9 +3,8 @@ package com.urise.webapp.storage;
 import com.urise.webapp.Config;
 
 public class SqlStorageTest extends AbstractStorageTest {
-    private static Config CONFIG = Config.get();
 
     public SqlStorageTest() {
-        super(new SqlStorage(CONFIG.getDbUrl(), CONFIG.getDbUser(), CONFIG.getDbPassword()));
+        super(Config.get().getStorage());
     }
 }
